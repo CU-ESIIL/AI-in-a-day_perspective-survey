a97efbbe75c7c908c2342b6b980bb664f1f572df
njlyon0 <nicholasjlyon@gmail.com>
2026-08-07 14:03:04 -0400

    fix (sandbox): update file paths for NL sandbox script

--- _sandbox/survey_lyon-explore.r
+++ _sandbox/survey_lyon-explore.r
@@ -9,17 +9,18 @@
 librarian::shelf(tidyverse, supportR)
 
 # Get set up
-source(file.path("scripts", "survey", "-setup.r"))
+source(file.path("-setup.r"))
 
 # Clear environment/collect garbage
 rm(list = ls()); gc()
 
 # Load any custom functions
-purrr::walk(.x = dir(path = file.path("scripts", "tools"), pattern = "*.r", full.names = TRUE),
+purrr::walk(.x = dir(path = file.path("tools"), 
+    pattern = "*.r", full.names = TRUE),
   .f = ~ source(file = .x))
 
 # Read in data
-svy_v01 <- read.csv(file.path("data", "survey-01_tidied.csv")) %>% 
+svy_v01 <- read.csv(file.path("data", "01_tidied-responses.csv")) %>% 
   dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
     .fns = ~ ifelse(nchar(.) == 0, yes = NA, no = .)))
 
@@ -27,7 +28,7 @@ svy_v01 <- read.csv(file.path("data", "survey-01_tidied.csv")) %>%
 dplyr::glimpse(svy_v01)
 
 # Read in survey question lookup table too
-lkup <- read.csv(file.path("data", "question-lookup-table.csv"))
+lkup <- read.csv(file.path("data", "01_question-lookup-table.csv"))
 # dplyr::glimpse(lkup)
 
 ## -------------------------------------------- ##
