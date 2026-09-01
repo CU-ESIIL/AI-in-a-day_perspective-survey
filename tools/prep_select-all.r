14ddf7689b93dd804fe651aca9464dc6519f39a1
njlyon0 <njlyon@alumni.iastate.edu>
2026-09-01 11:05:43 -0600

    fix (tool): remove broken conditional renaming response id column

--- tools/prep_select-all.r
+++ tools/prep_select-all.r
@@ -21,21 +21,12 @@ prep_select_all <- function(df = NULL, q = NULL, summarize = TRUE){
   if(!is.logical(summarize)){
     warning("'summarize' must be a logical. Coercing to TRUE")
     summarize <- TRUE }
-  
-  # response ID options
-  if(summarize == FALSE){
-    camel_case = FALSE # default
-    if('ResponseId' %in% names(df)){
-      camel_case <-TRUE
-      df <- df %>% rename(response_id = ResponseId)
-    }
-  }
-  
+    
   # Remove NAs in relevant question
   df_v02 <- df[!is.na(df[[q]]),]
   
   # Pare down columns
-  df_v03 <- dplyr::select(.data = df_v02, response_id, dplyr::all_of(q))
+  df_v03 <- dplyr::select(.data = df_v02, ResponseId, dplyr::all_of(q))
 
   # Handle difference between commas in actual response text versus collapsing char
   df_v04 <- df_v03 %>% 
@@ -50,7 +41,7 @@ prep_select_all <- function(df = NULL, q = NULL, summarize = TRUE){
 
   # Get one row per 'checked box' in original question
   df_v05 <- df_v04 %>% 
-    tidyr::separate_wider_delim(cols = -response_id, delim = ";",
+    tidyr::separate_wider_delim(cols = -ResponseId, delim = ";",
       names = c(paste0("box", 1:(max_delim + 1))), too_few = "align_start") %>% 
     tidyr::pivot_longer(cols = dplyr::starts_with("box")) %>% 
     dplyr::select(-name) %>% 
@@ -60,24 +51,19 @@ prep_select_all <- function(df = NULL, q = NULL, summarize = TRUE){
   df_v06 <- df_v05 %>% 
     dplyr::mutate(value = gsub(pattern = "’", replacement = "'", x = value))
   
-    
   # Summarize response data
-  if (summarize) {
+  if(summarize == TRUE){
   df_v07 <- df_v06 %>% 
     dplyr::mutate(total_respondents = length(unique(ResponseId))) %>% 
     dplyr::group_by(value, total_respondents) %>% 
     dplyr::summarize(unique_respondents = length(unique(ResponseId)),
       .groups = "drop") %>% 
     dplyr::mutate(percent = round((unique_respondents / total_respondents) * 100, digits = 1)) %>% 
-    dplyr::arrange(dplyr::desc(percent))
+    dplyr::arrange(dplyr::desc(percent)) 
+  
+  } else { df_v07 <- df_v06 }
   
   # Return it
-  return(df_v07) } else{
-    if(camel_case) {
-    df_v06 <- df_v06 %>%
-      rename(ResponseId = response_id)
-    }
-    return(df_v06)
-  }
-}
+  return(df_v07) }
+
 # End ----
