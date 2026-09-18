6bf723f2db07065b7ba94b7bc58d7f3f0f4bda62
njlyon0 <nicholasjlyon@gmail.com>
2026-09-18 15:10:57 -0400

    feat (tool): `prep_select_one` calcs relative percent for groups (per #3)

--- tools/prep_select-one.r
+++ tools/prep_select-one.r
@@ -27,63 +27,62 @@ prep_select_one <- function(df = NULL, q = NULL, grp = NULL){
   # Remove NAs in relevant question
   df_v02 <- df[!is.na(df[[q]]),]
 
-  # If desired, also remove NAs from grouping column(s)
+  # Also remove NAs from grouping column(s)
   if(is.null(grp) != TRUE){
     for(g in seq_along(grp)){
       df_v02 <- df_v02[!is.na(df_v02[[grp[g]]]),]
     }
   }
   
+  # Pare down columns
+  need_cols <- intersect(x = names(df_v02), y = c("ResponseId", q, grp))
+  df_v03 <- dplyr::select(.data = df_v02, dplyr::all_of(need_cols))
+  
   # Handle response order
   if(paste0(q, "__value") %in% names(df)){
-    df_v03 <- dplyr::arrange(.data = df_v02, dplyr::across(dplyr::starts_with(paste0(q, "__value"))))
-  } else { df_v03 <- dplyr::arrange(.data = df_v02, dplyr::across(dplyr::starts_with(q))) }
-
+    df_v04 <- dplyr::arrange(.data = df_v03, dplyr::across(dplyr::starts_with(paste0(q, "__value"))))
+  } else { df_v04 <- dplyr::arrange(.data = df_v03, dplyr::across(dplyr::starts_with(q))) }
+  
   # Make response into a factor
   if(is.factor(df[[q]])){
-    df_v04 <- df_v03
+    df_v05 <- df_v04
   } else {
-    df_v04 <- df_v03
-    df_v04[[q]] <- factor(x = df_v04[[q]], levels = unique(df_v04[[q]]))
+    df_v05 <- df_v04
+    df_v05[[q]] <- factor(x = df_v05[[q]], levels = unique(df_v05[[q]]))
   }
 
   # Make grouping variables a factor (if any are provided)
   if(is.null(grp) != TRUE){
     for(k in seq_along(grp)){
-      df_v04 <- df_v04 %>% 
+      df_v05 <- df_v05 %>% 
         dplyr::arrange(dplyr::across(dplyr::starts_with(paste0(grp[g], "__value"))))
-      df_v04[[grp[g]]] <- factor(x = df_v04[[grp[g]]], levels = unique(df_v04[[grp[g]]]))
+      df_v05[[grp[g]]] <- factor(x = df_v05[[grp[g]]], levels = unique(df_v05[[grp[g]]]))
     }
   }
 
-
+  # Count total respondents
+  df_v06 <- dplyr::mutate(.data = df_v05, total_respondents = length(unique(ResponseId)))
+  
   # Assign correct grouping structure
   if(is.null(grp) != TRUE){
-    df_v05 <- df_v04 %>% 
-      dplyr::group_by(dplyr::across(dplyr::all_of(c(grp, q))))
-  } else {
-    df_v05 <- df_v04 %>% 
-      dplyr::group_by(dplyr::across(dplyr::all_of(q)))
-  }
-
-  # Count responses per question * group category
-  df_v06 <- df_v05 %>% 
-    dplyr::summarize(response_count = dplyr::n(),
-      .groups = "drop")
-  
-  # Re-group as/if needed
-  if(is.null(grp) != TRUE){
     df_v07 <- df_v06 %>% 
-      dplyr::group_by(dplyr::across(dplyr::all_of(c(grp))))
+      dplyr::group_by(dplyr::across(dplyr::all_of(c(grp, "total_respondents")))) %>% 
+      dplyr::mutate(grp_respondents = length(unique(ResponseId))) %>% 
+      dplyr::ungroup()
   } else {
-    df_v07 <- df_v06
+    df_v07 <- dplyr::mutate(df_v06, grp_respondents = total_respondents)
   }
 
+  # Summarize response data
   df_v08 <- df_v07 %>% 
-    dplyr::mutate(total_responses = sum(response_count, na.rm = TRUE),
-      percent = round((response_count / total_responses) * 100, digits = 1)) %>% 
-    dplyr::ungroup()
-
+    dplyr::group_by(dplyr::across(dplyr::all_of(
+      c(grp, q, "total_respondents", "grp_respondents")))) %>% 
+    dplyr::summarize(unique_respondents = length(unique(ResponseId)),
+      .groups = "drop") %>% 
+    dplyr::mutate(percent = round((unique_respondents / total_respondents) * 100, digits = 1),
+      relative_percent = round((unique_respondents / grp_respondents) * 100, digits = 1)) %>% 
+    dplyr::arrange(dplyr::desc(percent))
+  
   # Return it
   return(df_v08) }
 
