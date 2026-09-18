fa289eac9516de35bab4862a0eca4625da04eca1
njlyon0 <nicholasjlyon@gmail.com>
2026-09-18 15:50:34 -0400

    feat (setup): create subfolders of 'graphs/'

--- -setup.r
+++ -setup.r
@@ -8,8 +8,12 @@
 # Clear environment/collect garbage
 rm(list = ls()); gc()
 
-# Make needed folder(s)
+# Make needed top-level folder(s)
 dir.create(path = file.path("data"), showWarnings = FALSE)
 dir.create(path = file.path("graphs"), showWarnings = FALSE)
+
+# Make necessary sub-folders of "graphs/"
+purrr::walk(.x = c("single-qs", "workshop-dev"),
+  .f = ~ dir.create(path = file.path("graphs", .x), showWarnings = FALSE))
 
 # End ----
